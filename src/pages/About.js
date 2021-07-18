6557db6813e8f589d2eea1bee118c79eb925da44
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-18 21:31:21 +1000

    my face

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <Hero
         title="Michelle Watts' Portfolio"
         subtitle="Hello! I am a digital campaigner at Unions NSW and a web developer. I have a background in graphic/web design and audio engineering. Check out some of my work by clicking the projects link in the navigation."
-        image="/img/my-face.jpeg"
+        image="./img/my-face.jpeg"
         ctaText="LinkedIn"
         ctaLink="https://www.linkedin.com/in/michelle-e-watts/"
         ctaText2="GitHub"
