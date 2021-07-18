e6ce3d1e82f59a506ae396ccca39cb60914d11f4
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-18 21:21:37 +1000

    hashrouter try 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Resume from "./pages/Resume";
 
 export default function App() {
   return (
-    <Router basename={"https://michellewatts20000.github.io/watts-react-portfolio-chakra/"}>
+    <Router>
       <Switch>
         <Route exact path = "/" component = {About}/>
         <Route path = "/portfolio" component = {Portfolio}/>
