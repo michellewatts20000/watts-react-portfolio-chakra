cb4b04fd641a0542e48c2c701ceed2be4362fb09
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-18 21:17:40 +1000

    basename

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ import Resume from "./pages/Resume";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename={"https://michellewatts20000.github.io/watts-react-portfolio-chakra/"}>
       <Switch>
-        <Route path = "/" component = {About}/>
+        <Route exact path = "/" component = {About}/>
         <Route path = "/portfolio" component = {Portfolio}/>
         <Route path = "/contact" component = {Contact}/>
         <Route path = "/resume" component = {Resume}/>
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import customTheme from "./utils/theme";
+import { BrowserRouter } from "react-router-dom"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={customTheme}>
+  <BrowserRouter>   
       <App />
+       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
   rootElement
