5ebeb02195fba4e2e0de7a06722378abb9e2a6fd
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-18 21:19:57 +1000

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+  HashRouter as Router,
+  Switch
 } from "react-router-dom"
 
 import About from "./pages/About";
