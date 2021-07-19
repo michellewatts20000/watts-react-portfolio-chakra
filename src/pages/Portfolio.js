476af6042cf1efec669e3d08b7b0fb9939ad6183
Michelle Watts <watts.e.michelle@gmail.com>
2021-07-19 14:58:20 +1000

    space in portfolio cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,72 +7,72 @@ import MainPage from "../components/layouts/MainPage";
 export default function Portfolio() {
   return (
     <MainPage>
-<Box>
-      <SimpleGrid columns={{ base: 1, md: 2, lg:3 }} spacing={5}>
-      
-   <Project
-          icon={"./img/calculator.png"}
-          title={'Unpaid overtime calculator'}
-          tech={"node.JS, MySQL"}
-          text={
-            'A full-stack app that calculates the amount of unpaid hours and salary you have worked over one year.'
-          }
-          link1={"https://unpaid-overtime.herokuapp.com"}
-          link2={"https://github.com/michellewatts20000/unpaid-overtime-calc"}
-        />
-        <Project
-         icon={"./img/pantry.png"}
-          title={"What's in my pantry?"}
-          tech={"HTML, CSS, JS, Bootstrap"}
-          text={
-            "What's in my pantry is an easy to use, all-in-one recipe search app that will display recipes based on a list of food items that are currently in your pantry."
-          }
-          link1={"https://michellewatts20000.github.io/whats-in-my-pantry/"}
-          link2={"https://github.com/michellewatts20000/whats-in-my-pantry"}
-        />
-        <Project
-         icon={"./img/blogger.png"} 
-          title={'Tech Blog'}
-          tech={"node.JS, MySQL"}
-          text={
-            'A full-stack app that lets you write and comment about tech related issues. Create an account, check out other peoples blogs.'
-          }
-          link1={"https://boiling-beach-99511.herokuapp.com/"}
-          link2={"https://github.com/michellewatts20000/tech-blog"}
-        />
-        <Project
-          icon={"./img/note.png"}
-          title={'Note Taker'}
-          tech={"node.JS, HTML, CSS"}
-          text={
-            'Write notes and save them for later. Keep track of tasks. Click on the delete button next to a note to delete from the applications memory.'
-          }
-           link1={"https://hidden-sands-80026.herokuapp.com/notes"}
-          link2={"https://github.com/michellewatts20000/note-taker"}
-        />
-        <Project
-          icon={"./img/weather.png"}
-          title={'Weather dashboard'}
-          tech={"API, JS, HTML, CSS"}
-          text={
-            'The weather dashboard is an app that gives you the current weather and UV index for a place you search for and a 5 day forecast.'
-          }
-          
-          link1={"https://michellewatts20000.github.io/weather-dashboard"}
-          link2={"https://github.com/michellewatts20000/weather-dashboard"}
-        />
-        <Project
-          icon={"./img/code-img-land.png"}
-          title={'Code quiz'}
-          tech={"JS, HTML, CSS"}
-          text={
-            'You have 75 seconds to answer 5 coding questions. Once you have finished the quiz you will be asked to enter your initials to the high scores.'
-          }
-          link1={"https://michellewatts20000.github.io/code-quiz/"}
-          link2={"https://github.com/michellewatts20000/code-quiz"}
-        />
-       </SimpleGrid>
-    </Box>
+      <Box>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} mb="5">
+
+          <Project
+            icon={"./img/calculator.png"}
+            title={'Unpaid overtime calculator'}
+            tech={"node.JS, MySQL"}
+            text={
+              'A full-stack app that calculates the amount of unpaid hours and salary you have worked over one year.'
+            }
+            link1={"https://unpaid-overtime.herokuapp.com"}
+            link2={"https://github.com/michellewatts20000/unpaid-overtime-calc"}
+          />
+          <Project
+            icon={"./img/pantry.png"}
+            title={"What's in my pantry?"}
+            tech={"HTML, CSS, JS, Bootstrap"}
+            text={
+              "What's in my pantry is an easy to use, all-in-one recipe search app that will display recipes based on a list of food items that are currently in your pantry."
+            }
+            link1={"https://michellewatts20000.github.io/whats-in-my-pantry/"}
+            link2={"https://github.com/michellewatts20000/whats-in-my-pantry"}
+          />
+          <Project
+            icon={"./img/blogger.png"}
+            title={'Tech Blog'}
+            tech={"node.JS, MySQL"}
+            text={
+              'A full-stack app that lets you write and comment about tech related issues. Create an account, check out other peoples blogs.'
+            }
+            link1={"https://boiling-beach-99511.herokuapp.com/"}
+            link2={"https://github.com/michellewatts20000/tech-blog"}
+          />
+          <Project
+            icon={"./img/note.png"}
+            title={'Note Taker'}
+            tech={"node.JS, HTML, CSS"}
+            text={
+              'Write notes and save them for later. Keep track of tasks. Click on the delete button next to a note to delete from the applications memory.'
+            }
+            link1={"https://hidden-sands-80026.herokuapp.com/notes"}
+            link2={"https://github.com/michellewatts20000/note-taker"}
+          />
+          <Project
+            icon={"./img/weather.png"}
+            title={'Weather dashboard'}
+            tech={"API, JS, HTML, CSS"}
+            text={
+              'The weather dashboard is an app that gives you the current weather and UV index for a place you search for and a 5 day forecast.'
+            }
+
+            link1={"https://michellewatts20000.github.io/weather-dashboard"}
+            link2={"https://github.com/michellewatts20000/weather-dashboard"}
+          />
+          <Project
+            icon={"./img/code-img-land.png"}
+            title={'Code quiz'}
+            tech={"JS, HTML, CSS"}
+            text={
+              'You have 75 seconds to answer 5 coding questions. Once you have finished the quiz you will be asked to enter your initials to the high scores.'
+            }
+            link1={"https://michellewatts20000.github.io/code-quiz/"}
+            link2={"https://github.com/michellewatts20000/code-quiz"}
+          />
+        </SimpleGrid>
+      </Box>
     </MainPage>
   );
 }
