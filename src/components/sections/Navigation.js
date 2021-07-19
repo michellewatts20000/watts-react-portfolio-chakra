9428fec552ea556da5613588622522cb9e00946e
Michelle Watts <watts.e.michelle@gmail.com>
2021-07-19 14:41:32 +1000

    link

--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -6,7 +6,7 @@ import { Text } from "@chakra-ui/react"
 const Navigation = (props) => {
   const { children, isLast, to = "/", ...rest } = props
   var pathArray = window.location.pathname.split('/');
-  var newPath = "/" + pathArray[2];
+  var newPath = "/" + pathArray[0];
   return (
     <Text
       mb={{ base: isLast ? 0 : 8, sm: 0 }}
